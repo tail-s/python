8dd84473c06df2873248d8ec4e30ad36703582f5
Sojin <voise.ksing@gmail.com>
2022-03-24 03:32:54 +0900

    22.03.24

--- 16_Object Oriented Programming/2_coffee machine/main.py
+++ 16_Object Oriented Programming/2_coffee machine/main.py
@@ -2,3 +2,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+def main():
+    select_menu = input("What would you like? (espresso/latte/cappuccino/) : ")
+    print(select_menu)
+    if select_menu == "off":
+        pass
+    elif select_menu == "report":
+        CoffeeMaker().report()
+    else:
+        return select_menu
+
+main()
