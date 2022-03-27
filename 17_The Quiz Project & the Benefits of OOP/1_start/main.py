6b8bc5ba99441b230ff4fb3f2679cc5a4e921453
Sojin <voise.ksing@gmail.com>
2022-03-27 23:29:33 +0900

    22.03.27

--- 17_The Quiz Project & the Benefits of OOP/1_start/main.py
+++ 17_The Quiz Project & the Benefits of OOP/1_start/main.py
@@ -1,9 +1,18 @@
-class User:
-    pass
-
-user_1 = User()
-
 # PascalCase / camelCase / snake_case
 
-# 클래스의 속성
-user_1.id = "001"
+class User:
+    def __init__(self, user_id, username):
+        # print("new user being created")
+        self.id = user_id
+        self.username = username
+        self.followers = 0 # default value
+
+user_1 = User("001", "Sojin")
+user_2 = User("002", "Jack")
+
+print(user_1.followers)
+
+
+
+
+
