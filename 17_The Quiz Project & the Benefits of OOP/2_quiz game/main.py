17e09ba96d06dd96213910fd7b6b9ad6c13c975a
Sojin <voise.ksing@gmail.com>
2022-03-28 02:24:42 +0900

    22.03.28

---
+++ 17_The Quiz Project & the Benefits of OOP/2_quiz game/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+# q_num = len(question_data) 굳이 이렇게 안해도 됨
+
+# for i in range(q_num):
+#     q = question_data[i]["text"]
+#     a = question_data[i]["answer"]
+#     data_i = Question(q, a)
+#     question_bank.append(data_i)
+
+for question in question_data: # 그냥 하나씩 넣어서
+    q_text = question["text"]
+    q_answer = question["answer"]
+    q_set = Question(q_text, q_answer)
+    question_bank.append(q_set)
+
+print(question_bank[0].test)
---
+++ 17_The Quiz Project & the Benefits of OOP/2_quiz game/question_model.py
@@ -0,0 +1,7 @@
+# attributes : test, answer
+
+class Question():
+
+    def __init__(self, q_test, q_answer):
+        self.test = q_test
+        self.answer = q_answer
