5038e3c2005afb211532654621e679e2c26d4ab1
Sojin <voise.ksing@gmail.com>
2022-03-30 00:27:57 +0900

    22.03.30

--- 18_Turtle & the Graphical User Interface (GUI)/1_start/main.py
+++ 18_Turtle & the Graphical User Interface (GUI)/1_start/main.py
@@ -1,3 +1,12 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("green")
+
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(50)
+
+screen = Screen()
+screen.exitonclick()
