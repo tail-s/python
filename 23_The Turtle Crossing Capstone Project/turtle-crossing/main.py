e33c208bce7e3829c02764bb9bf14506c31cdf84
Sojin <voise.ksing@gmail.com>
2022-04-20 00:48:48 +0900

    22.04.20 작성중

--- 23_The Turtle Crossing Capstone Project/turtle-crossing/main.py
+++ 23_The Turtle Crossing Capstone Project/turtle-crossing/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.up, "Up")
+screen.onkey(player.down, "Down")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    player.clear()
--- 23_The Turtle Crossing Capstone Project/turtle-crossing/player.py
+++ 23_The Turtle Crossing Capstone Project/turtle-crossing/player.py
@@ -8,5 +8,23 @@ class Player(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.color("white")
+        self.shape("turtle")
+        self.setheading(90)
         self.penup()
+        self.reset_position()
+
+    def reset_position(self):
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def clear(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            self.reset_position()
+
