7af0271e7d3abb40557706a456c891a702de6851
Sojin <voise.ksing@gmail.com>
2022-04-20 00:02:32 +0900

    22.04.20 작성중

---
+++ 23_The Turtle Crossing Capstone Project/turtle-crossing/player.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
