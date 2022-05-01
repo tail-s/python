89458f66f29fb6756ffe05fec89c85b677069fc6
Sojin <voise.ksing@gmail.com>
2022-05-02 00:30:34 +0900

    22.05.02 판다스 재밌다

--- 25_Working with CSV Data and the Pandas Library/main.py
+++ 25_Working with CSV Data and the Pandas Library/main.py
@@ -2,13 +2,36 @@
 #     list = w_data.readlines()
 #     print(list)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as w_data:
+#     data = csv.reader(w_data)
+#     temp = []
+#     for row in data:
+#         t = row[1]
+#         temp.append(t)
+#     temp.pop(0)
+#     print(temp)
 
-with open("weather_data.csv") as w_data:
-    data = csv.reader(w_data)
-    temp = []
-    for row in data:
-        t = row[1]
-        temp.append(t)
-    temp.pop(0)
-    print(temp)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+
+# # print(data["temp"])
+# # print(type(data))
+#
+# data_dict = data.to_dict()
+# # print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# # print(temp_list)
+#
+# avg = sum(temp_list) / len(temp_list)
+# print(avg)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# print(data.temp)
+
+# print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
