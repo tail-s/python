19ee6e09795126e3f8901dc7d71bd1a1092f6d58
Sojin <voise.ksing@gmail.com>
2022-05-05 01:23:28 +0900

    22.05.05

--- 25_Working with CSV Data and the Pandas Library/2_Squirrel/main.py
+++ 25_Working with CSV Data and the Pandas Library/2_Squirrel/main.py
@@ -3,5 +3,12 @@ import pandas
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 gray = data[data["Primary Fur Color"] == "Gray"]
-# gray = data[data["Primary Fur Color"] == "Gray"]
-# gray = data[data["Primary Fur Color"] == "Gray"]
+cin = data[data["Primary Fur Color"] == "Cinnamon"]
+black = data[data["Primary Fur Color"] == "Black"]
+
+new_data = {
+    "Fur Color": ["gray", "red", "black"],
+    "Count": [len(gray), len(cin), len(black)]
+}
+ans = pandas.DataFrame(new_data)
+ans.to_csv("squirrel_count.csv")
