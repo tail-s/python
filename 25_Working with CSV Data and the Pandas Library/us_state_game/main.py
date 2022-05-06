06bde0a4e5e2b0edce6943ca4b793a60039672e7
Sojin <voise.ksing@gmail.com>
2022-05-06 19:47:27 +0900

    22.05.06

--- 25_Working with CSV Data and the Pandas Library/us_state_game/main.py
+++ 25_Working with CSV Data and the Pandas Library/us_state_game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -6,8 +7,31 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+state_list = data["state"].to_list()
+ansd_list = []
 
+game_is_on = True
+
+while game_is_on:
+    answer_state = screen.textinput(title=f"{len(ansd_list)}/{len(state_list)} Guess the State", prompt="What's another state's name?")
+
+    if answer_state in state_list:
+        ansd_list.append(answer_state)
+
+        target_x = int(data[data.state == f"{answer_state}"].x)
+        target_y = int(data[data.state == f"{answer_state}"].y)
+
+        ans = turtle.Turtle()
+        ans.penup()
+        ans.hideturtle()
+        ans.goto(target_x, target_y)
+        ans.write(f"{answer_state}", align="center", font=("Courier", 10, "normal"))
+
+        if len(ansd_list) == len(state_list):
+            game_is_on = False
+
+    screen.update()
 
 
 turtle.mainloop()
