9a14625f73b82e4b46c210592d506798311124ca
Sojin <voise.ksing@gmail.com>
2022-05-11 20:40:01 +0900

    22.05.11

--- 26_List Comprehension and the NATO Alphabet/1_start/test.py
+++ 26_List Comprehension and the NATO Alphabet/1_start/test.py
@@ -17,4 +17,6 @@ import random
 students_scores = {student:random.randint(1, 100) for student in names}
 passed_students = {student:score for (student, score) in students_scores.items() if score >= 60}
 
-
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word:len(word) for word in sentence.split()}
+print(result)
