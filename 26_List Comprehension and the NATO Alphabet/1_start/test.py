a0dda65fa8ab8af2403c041eebbe13c06295e5f2
Sojin <voise.ksing@gmail.com>
2022-05-17 23:34:37 +0900

    22.05.17

--- 26_List Comprehension and the NATO Alphabet/1_start/test.py
+++ 26_List Comprehension and the NATO Alphabet/1_start/test.py
@@ -19,4 +19,15 @@ passed_students = {student:score for (student, score) in students_scores.items()
 
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 result = {word:len(word) for word in sentence.split()}
-print(result)
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {day:(temp_c * 9/5) + 32 for (day, temp_c) in weather_c.items()}
+print(weather_f)
