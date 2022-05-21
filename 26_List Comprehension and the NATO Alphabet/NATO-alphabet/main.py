b1197d4c4910c860830a88eb8483788baf348d8e
Sojin <voise.ksing@gmail.com>
2022-05-21 23:19:59 +0900

    22.05.21

--- 26_List Comprehension and the NATO Alphabet/NATO-alphabet/main.py
+++ 26_List Comprehension and the NATO Alphabet/NATO-alphabet/main.py
@@ -25,4 +25,6 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
