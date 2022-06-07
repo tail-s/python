5b5826b639485bb215df0c70234fdfa4bdbe698c
Sojin <voise.ksing@gmail.com>
2022-06-07 09:50:35 +0900

    22.06.07

---
+++ 27_Tkinter, args, kwargs and Creating GUI Programs/main.py
@@ -0,0 +1,24 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+#Label
+
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
